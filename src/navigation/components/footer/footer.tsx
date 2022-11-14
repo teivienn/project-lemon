45a85db6c881707186ec28b0cb42205aa425a442
Vitaly Statsura <vstatsura@stagewood.com>
2022-11-14 22:29:27 +0300

    fix footer

--- src/navigation/components/footer/footer.tsx
+++ src/navigation/components/footer/footer.tsx
@@ -7,24 +7,46 @@ export const Footer = () => {
   const { t } = useTranslation();
   return (
     <Box bg="#000" py="40px">
-      <Container justify="flex-end" alignItems="center" display="flex" direction="column">
-        <Box display="flex" justifyContent="space-between" flex={1} width="100%">
+      <Container
+        justify="flex-end"
+        alignItems="center"
+        display="flex"
+        direction="column"
+        xl
+      >
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          flex={1}
+          width="100%"
+          alignItems="flex-start"
+        >
           <Box display="flex" flexDirection="column">
-            <Text color="$white" b>
+            <Text hideIn="sm" color="$white" b>
               ООО «Профикарлайн»
             </Text>
-            <Text color="$white">220036, г. Минск, ул. К. Либкнехта,</Text>
-            <Text color="$white">66-150 (7 этаж, левое крыло)</Text>
+            <Text hideIn="sm" color="$white">
+              220036, г. Минск, ул. К. Либкнехта,
+            </Text>
+            <Text hideIn="sm" color="$white">
+              66-150 (7 этаж, левое крыло)
+            </Text>
           </Box>
 
           <Box display="flex" flexDirection="column" alignItems="flex-end">
-            <Text color="$white">Тел./факс: +375 (17) 222-11-02 (многоканальный)</Text>
-            <Text color="$white">Тел.: +375 (29) 122-11-24</Text>
-            <Text color="$white">Email: info@pcline.by</Text>
+            <Text hideIn="sm" color="$white">
+              Тел./факс: +375 (17) 222-11-02 (многоканальный)
+            </Text>
+            <Text hideIn="sm" color="$white">
+              Тел.: +375 (29) 122-11-24
+            </Text>
+            <Text hideIn="sm" color="$white">
+              Email: info@pcline.by
+            </Text>
           </Box>
         </Box>
 
-        <Box center display="flex" flexDirection="column" pt={50}>
+        <Box center display="flex" flexDirection="column">
           <Box display="flex" alignItems="center" flexDirection="column">
             <Icons.Logo fill="#fff" />
             <Text color="$white" b>
