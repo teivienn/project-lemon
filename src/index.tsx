15a3382cda79b0545a06af75f74544b2cd39075c
Vitaly Statsura <vstatsura@stagewood.com>
2022-11-15 01:32:48 +0300

    pwa2

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import * as serviceWorkerRegistration from './services/workers/serviceWorkerRegistration';
-import reportWebVitals from './services/workers/reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { App } from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -16,8 +15,3 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
